1728ecefde7dce7bb423aaebd3130b17fc62ff08
101380164 <1013801464@qq.com>
2018-04-29 19:53:13 +0800

    供应商管理彻底完成

---
+++ 企业信息管理/addsupplier.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace 企业信息管理 {
+    public partial class addsupplier : System.Web.UI.Page {
+        string connectionStr = ConfigurationManager.ConnectionStrings["access2"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e) {
+            if (Session["username"] == null) {
+                Response.Redirect("login.aspx");
+                return;
+            }
+            username.Text = Session["nickname"] as string;
+            comment.Text = Session["comment"] as string;
+        }
+
+        protected void btnCommit_Click(object sender, EventArgs e) {
+            using (OleDbConnection conn = new OleDbConnection(connectionStr)) {
+                using (OleDbCommand cmd = new OleDbCommand("insert into supplier(sup_name) values(\'" + tbSupplierName.Text + "\')", conn)) {
+                    conn.Open();
+                    int affectedRows = cmd.ExecuteNonQuery();
+                    outputBasicJavascriptLib();
+                    if (affectedRows > 0) {
+                        Response.Write("<script>$(document).ready(function(){swal(\"修改成功\", \"\", \"success\");})</script>");
+                    } else
+                        Response.Write("<script>$(document).ready(function(){swal(\"修改失败\", \"\", \"error\");})</script>");
+
+                }
+            }
+        }
+
+        private void outputBasicJavascriptLib() {
+            Response.Write("<script src=\"js/jquery-2.1.4.min.js\"></script>\n");
+            Response.Write("<script type=\"text/javascript\" src=\"js/plugins/sweetalert.min.js\"></script>\n");
+        }
+    }
+}
--- 企业信息管理/suppliers.aspx.cs
+++ 企业信息管理/suppliers.aspx.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.OleDb;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace 企业信息管理 {
@@ -25,27 +21,20 @@ namespace 企业信息管理 {
         protected void showCompanyList() {
             if (!IsPostBack) {
                 using (OleDbConnection conn = new OleDbConnection(connectionStr)) {
-                    using (OleDbDataAdapter adapter = new OleDbDataAdapter("select * from dbo.supplier", conn)) {
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter("select * from supplier", conn)) {
                         DataSet ds = new DataSet();
                         adapter.Fill(ds);
                         gvCompanyList.DataSource = ds;
                         gvCompanyList.DataBind();
-                        if (gvCompanyList.Rows.Count > 0) {
-                            // 使用<TH>替换<TD>
-                            gvCompanyList.UseAccessibleHeader = true;
-                            //HeaderRow将被<thead>包裹，数据行将被<tbody>包裹
-                            gvCompanyList.HeaderRow.TableSection = TableRowSection.TableHeader;
-                            // FooterRow将被<tfoot>包裹
-                            gvCompanyList.FooterRow.TableSection = TableRowSection.TableFooter;
-                        }
+                        standardTable();
                     }
                 }
             }
         }
-        
+
         protected void Delete(int supId) {
-            using(OleDbConnection conn = new OleDbConnection(connectionStr)) {
-                using(OleDbCommand cmd = new OleDbCommand("delete from dbo.supplier where sup_id=" + supId, conn)) {
+            using (OleDbConnection conn = new OleDbConnection(connectionStr)) {
+                using (OleDbCommand cmd = new OleDbCommand("delete from supplier where sup_id=" + supId, conn)) {
                     conn.Open();
                     cmd.ExecuteNonQuery();
                 }
@@ -60,13 +49,51 @@ namespace 企业信息管理 {
         }
 
         protected void gv_company_list_RowCommand(object sender, GridViewCommandEventArgs e) {
-            if(e.CommandName.Equals("Update")) {
-                // Convert the row index stored in the CommandArgument
-                // property to an Integer.
+            if (e.CommandName.Equals("XX")) {
                 int index = Convert.ToInt32(e.CommandArgument);
+                Session["selectedIndex"] = index;
                 GridViewRow selectedRow = gvCompanyList.Rows[index];
-                // Display the selected author.
+                tbID.Text = selectedRow.Cells[0].Text;
+                tbCompanyName.Text = selectedRow.Cells[2].Text;
+                standardTable();
             }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e) {
+            if (Session["selectedIndex"] == null) return;
+            using (OleDbConnection conn = new OleDbConnection(connectionStr)) {
+                using (OleDbCommand cmd = new OleDbCommand("update supplier set sup_name='" + tbCompanyName.Text + "' where sup_id=" + tbID.Text, conn)) {
+                    conn.Open();
+                    int affectedRows = cmd.ExecuteNonQuery();
+                    outputBasicJavascriptLib();
+                    if (affectedRows > 0) {
+                        try {
+                            GridViewRow selectedRow = gvCompanyList.Rows[(int)Session["selectedIndex"]];
+                            selectedRow.Cells[2].Text = tbCompanyName.Text;
+                            Session.Remove("selectedIndex");
+                        } catch (Exception) { }
+                        Response.Write("<script>$(document).ready(function(){swal(\"修改成功\", \"\", \"success\");})</script>");
+                    } else
+                        Response.Write("<script>$(document).ready(function(){swal(\"修改失败\", \"\", \"error\");})</script>");
+                    standardTable();
+                }
+            }
+        }
+
+        // 修改属性, 生成带有thead和tfoot的标准table
+        private void standardTable() {
+            if (gvCompanyList.Rows.Count > 0) {
+                // 使用<TH>替换<TD>
+                gvCompanyList.UseAccessibleHeader = true;
+                //HeaderRow将被<thead>包裹，数据行将被<tbody>包裹
+                gvCompanyList.HeaderRow.TableSection = TableRowSection.TableHeader;
+                // FooterRow将被<tfoot>包裹
+                gvCompanyList.FooterRow.TableSection = TableRowSection.TableFooter;
+            }
+        }
+
+        private void outputBasicJavascriptLib() {
+            Response.Write("<script src=\"js/jquery-2.1.4.min.js\"></script>");
         }
     }
 }
